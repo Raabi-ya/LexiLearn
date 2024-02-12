4caf2a19b07571bdf4bc9a21b740e3732b4f3ffc
sarinihasangee <157481833+sarinihasangee@users.noreply.github.com>
2024-02-12 17:34:32 +0530

    Create Home.js

---
+++ lexilearn/src/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="home">
+    <div className="main-content">
+      <h1>LexiLearn</h1>
+      <p><b>Personalized Web Based Educational Support Application for Dyslexic Children </b></p>
+      <ul>
+        <li>Personalised learning path</li>
+        <li>Real time motivation in text and speech</li>
+        <li>Gamified levels</li>
+        <li>Reading alphabets</li>
+        <li>Progress tracking</li>     
+      </ul>
+    </div>
+    <div className="image">
+      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Flanguagetool.org%2Finsights%2Fpost%2Fhow-many-letters-english-alphabet%2F&psig=AOvVaw3Q31E5Ul8MsXs_JuVTgpzZ&ust=1707728947711000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLD_6p34ooQDFQAAAAAdAAAAABAE" alt="LexiLearn" />
+    </div>
+  </div>
+);
+
+export default Home;
