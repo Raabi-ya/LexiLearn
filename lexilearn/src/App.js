fd17184bafac1ce06c682b6ea8c6d0e5ea491e54
sarinihasangee <157481833+sarinihasangee@users.noreply.github.com>
2024-02-12 17:49:20 +0530

    Navbar.js and App.js
    added Home to the App.js

--- lexilearn/src/App.js
+++ lexilearn/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import LoginPage from './LoginPage';
+import Home from './Home';
     function App() {
       return (
         <div className="App">
           <LoginPage />
+          <Home />
         </div>
       );
     }
---
+++ lexilearn/src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => (
+  <div className="navbar">
+    <a href="#home">Home</a>
+    <a href="#levels">Levels</a>
+    <a href="#userForm">User Profile</a>
+    <a href="#aboutUs">About Us</a>
+    <a href="#signUp">Sign Up</a>
+    <a href="#signIn">Sign In</a>
+  </div>
+);
+
+export default Navbar;
