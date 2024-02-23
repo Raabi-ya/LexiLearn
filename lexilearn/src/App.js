1021d6b3de05437a06a495bbbf25188eac8a8fbb
hiranya22 <hira.ranasinghe22@gmail.com>
2024-02-23 18:52:30 +0530

    about us page commit

--- lexilearn/src/App.js
+++ lexilearn/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LoginPage from './LoginPage';
-import UserForm from './userForm'; // Assuming UserForm component is imported correctly
+import UserForm from './userForm'; 
+import AbouUsPage from './AboutUsPage';
 
 function App() {
   return (
     <div className="App">
       <LoginPage />
       {/*<UserForm />*/}
+      <AbouUsPage />
     </div>
   );
 }
