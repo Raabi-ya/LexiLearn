b9b3ba125c899e77a069d0fdcd45cb76402c487d
hiranya22 <hira.ranasinghe22@gmail.com>
2024-03-02 10:07:20 +0530

    Update index.jsx

--- lexilearn/src/contexts/authContexts/index.jsx
+++ lexilearn/src/contexts/authContexts/index.jsx
@@ -1,6 +1,6 @@
-import React, { useContext, useState, useEffect } from "react";
-import { auth } from "../../firebase/firebase";
-import { onAuthStateChanged } from "firebase/auth";
+import React, { useContext, useState, useEffect } from 'react';
+import { auth } from '../../firebase/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 
 const AuthContext = React.createContext();
 
@@ -10,31 +10,18 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null);
-  const [userLoggedIn, setUserLoggedIn] = useState(false);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, initializeUser);
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
     return unsubscribe;
   }, []);
 
-  async function initializeUser(user) {
-    if (user) {
-
-      setCurrentUser({ ...user });
-      setUserLoggedIn(true);
-    } else {
-      setCurrentUser(null);
-      setUserLoggedIn(false);
-    }
-
-    setLoading(false);
-  }
-
   const value = {
-    currentUser,
-    userLoggedIn,
-    loading
+    currentUser
   };
 
   return (
