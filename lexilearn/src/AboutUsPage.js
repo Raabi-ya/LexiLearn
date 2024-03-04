627bbde19cc0b0bd7e995d69a08b813092a89d19
hiranya22 <hira.ranasinghe22@gmail.com>
2024-03-04 21:51:49 +0530

    Updating AboutUsPage

--- lexilearn/src/AboutUsPage.js
+++ lexilearn/src/AboutUsPage.js
@@ -10,6 +10,9 @@ const AboutUsPage = () => {
       <h1>About Us</h1>
       <section className="section">
         <h2 className="section-title">Our Story</h2>
+        <div className='image-div'>
+          <img src="./story.png" alt='Graphic art'/>
+        </div>
         <p className="section-paragraph">
           Our story begins with the recognition of the lack of awareness and limited educational support for dyslexic students, leading to high dropout rates and academic challenges. 
           Motivated by the desire to make a positive impact, we developed a personalised, gamified web application to provide much-needed assistance to dyslexic learners.
@@ -18,6 +21,9 @@ const AboutUsPage = () => {
       
       <section className="section">
         <h2 className="section-title">Our Team</h2>
+        <div className='image-div'>
+          <img src="./team.png" alt='Graphic art'/>
+        </div>
         <p className="section-paragraph">
           We are a team of enthusiastic computer science undergraduates studying at the Informatics Institute of Technology. 
           Our dedication to this project stems from our academic pursuits, 
@@ -26,6 +32,9 @@ const AboutUsPage = () => {
       </section>
       <section className="section">
         <h2 className="section-title">Our Mission</h2>
+        <div className='image-div'>
+          <img src="./mission.png" alt='Graphic art'/>
+        </div>
         <p className="section-paragraph">
           Our mission is to address the critical need for personalized assistance for dyslexic children through a web application that provides essential tools and tailored support to meet their unique needs. 
           Our goal is to alleviate the reading challenges encountered by dyslexic learners and foster inclusive education.
@@ -34,6 +43,9 @@ const AboutUsPage = () => {
 
       <section className="section">
         <h2 className="section-title">Our Values</h2>
+        <div className='image-div'>
+          <img src="./values.png" alt='Graphic art'/>
+        </div>
         <p className="section-paragraph">
           Our values are rooted in the belief that every child deserves equal access to education and opportunities for academic success. 
           We are committed to leveraging technology to create a supportive learning environment that empowers dyslexic students to excel. 
