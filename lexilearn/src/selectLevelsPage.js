94ea7d697612a90cdbb38c97df0a86c56614f0eb
hiranya22 <hira.ranasinghe22@gmail.com>
2024-03-08 18:58:52 +0530

    Level 1 committing

--- lexilearn/src/selectLevelsPage.js
+++ lexilearn/src/selectLevelsPage.js
@@ -1,38 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './SelectLevelsPage.css';
 import Footer from './Footer';
+import Level1 from './Level1'; // Import Level1Page component
+
 function SelectLevelsPage() {
-  const [SelectedLevel, setSelectedLevel] = useState(null);
-
-  const handleLevelSelect = (level) => {
-    setSelectedLevel(level);
-    console.log(`Selected Level ${level}`);
-  };
-
   return (
     <div>
-    <div className="Selected-Levels-Page">
-      <header className="Selected-Levels-Page-header">
-        <div className="content">
-          <div className="Level-button">
-            <h1>Levels</h1>
-            <p>Select Level to Play :</p>
-            
-            <button onClick={() => handleLevelSelect(1)}>Pre Test</button>
-            <button onClick={() => handleLevelSelect(2)}>Level 1</button>
-            <button onClick={() => handleLevelSelect(3)}>Level 2</button>
-            <button onClick={() => handleLevelSelect(4)}>Level 3</button>
+      <div className="Selected-Levels-Page">
+        <header className="Selected-Levels-Page-header">
+          <div className="content">
+            <div className="Level-button">
+              <h1>Levels</h1>
+              <p>Select Level to Play :</p>
+              <div>
+                <Link to="/pretest"><button>Pre Test</button></Link>
+                <Link to="/Level1"><button>Level 1</button></Link>
+                <Link to="/level2"><button>Level 2</button></Link>
+                <Link to="/level3"><button>Level 3</button></Link>
+            </div>
+            </div>
+            <div className="image">
+              <img src="/climb.png" alt="Climbing" />
+            </div>
+            <div className="image2">
+              <img src="/ladder.png" alt="Ladder" />
+            </div>
           </div>
-          <div className="image">
-            <img src="/climb.png" alt="Climbing" />
-          </div>
-          <div className="image2">
-          <img src="/ladder.png" alt="Ladder" />
-          </div>
+        </header>
       </div>
-      </header>
-    </div>
-    <div>< Footer /></div>
+      <Footer />
     </div>
   );
 }
