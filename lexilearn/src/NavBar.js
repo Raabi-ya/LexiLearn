606efbb0afc7ee58ff0db1ab813c25caf36409b7
sarinihasangee <157481833+sarinihasangee@users.noreply.github.com>
2024-03-15 13:10:11 +0530

    Welcome message with email

--- lexilearn/src/NavBar.js
+++ lexilearn/src/NavBar.js
@@ -1,16 +1,14 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from './context/AuthContext';
-import { auth,db } from "./firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { auth } from "./firebase";
 import "./NavBar.css";
 
 function Navbar() {
   const [openLinks, setOpenLinks] = useState(false);
   const { currentUser, dispatch } = useContext(AuthContext);
   const { navigate } = useNavigate();
-  const [username, setUsername] = useState("");
 
   const toggleNavbar = () => {
     setOpenLinks(!openLinks);
@@ -24,28 +22,6 @@ function Navbar() {
       console.error('Error signing out:', error);
     });
   }
-
-  const getUser = async () => {
-    const user = auth.currentUser;
-    if(user){
-    const docRef = doc(db, "users", user.uid);
-    const docSnap = await getDoc(docRef);
-
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-      const userData = docSnap.data();
-      setUsername(userData.username);
-    } else {
-     // docSnap.data() will be undefined in this case
-     console.log("No such document!");
-    }
-    
-  }
-  };
-
-  useEffect(() => {
-    getUser()
-  }, []);
 
   return (
     <div className={`navbar ${openLinks ? "open" : "close"}`}>
@@ -67,7 +43,7 @@ function Navbar() {
           <Link to="/LoginPage">Login</Link>
         )}*/}
 
-          {currentUser && <span>Welcome, {username}</span>}
+          {currentUser && <span>Welcome, {currentUser.email}</span>}
           {currentUser && <Link to="#" onClick={handleSignOut}>Sign Out</Link>}
           {!currentUser && <Link to="/LoginPage">Login</Link>}
 
@@ -90,7 +66,7 @@ function Navbar() {
           <Link to="/LoginPage">Login</Link>
         )}*/}
 
-        {currentUser && <span>Welcome, {username}</span>}
+        {currentUser && <span>Welcome, {currentUser.email}</span>}
         {currentUser && <Link to="#" onClick={handleSignOut}>Sign Out</Link>}
         {!currentUser && <Link to="/LoginPage">Login</Link>}
 
