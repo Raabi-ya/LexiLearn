134a150c1bf31898ebb014ce13d97fa0c4a64898
Raabi-ya <fathima.20222116@iit.ac.lk>
2024-03-19 11:26:40 +0530

    Updated SelectLevelsPage with Images

--- lexilearn/src/SelectLevelsPage.js
+++ lexilearn/src/SelectLevelsPage.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './SelectLevelsPage.css';
 import Footer from './Footer';
 
 function SelectLevelsPage() {
- 
+  const [backgroundImage] = useState([
+    './classroompath.jpg',
+    '/bookpath.png',
+    '/roadpath.jpg',
+    './beachpath.jpg',
+    './forestpath.jpg',
+    './bridgepath.jpg',
+    './candypath.jpg',
+    './frozenpath.jpg',
+    './animalspath.jpg',
+    './carspath.jpg', 
+    './playgroundpath.jpg',
+    './dogspath.jpg',
+    './bearspath.jpg',
+    './castlepath.jpg',
+    './baymaxpath.jpg',
+    './footballpath.jpeg',
+    './cricketpath.jpg',
+    'flintstonespath.png',
+    'princesspath.jpg',
+    './ralphpath.jpg',
+    './garfieldpath.jpg',
+    './animebridgepath.jpg'] );
+  const [currentBackgroundIndex, setCurrentBackgroundIndex] = useState(0);
+
+  const handleBackgroundChange = () => {
+    setCurrentBackgroundIndex((prevIndex) => (prevIndex + 1) % backgroundImage.length);
+    
+  };
+
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImage[currentBackgroundIndex]})`, // Path to the current background image
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    minHeight: '100vh',
+    /* Add any other styles you need */
+  };
+
 
   return (
-      <div>
+      <div style={backgroundStyle}>
       <div className="Selected-Levels-Page">
         <header className="Selected-Levels-Page-header">
           <div className="content">
@@ -15,23 +52,18 @@ function SelectLevelsPage() {
             <div className="Level-button">
               <h1>Levels</h1>
               <p>Select Level to Play :</p>
-              <div>
-                <ul>
-                <li><Link to="/PlacementTest"><button className="image1-button">
-                <img src="/pretest.png" alt="Level 1" /></button></Link></li>
-                <li><Link to="/Level1"><button>Level 1</button></Link></li>
-                <li><Link to="/level2"><button>Level 2</button></Link></li>
-                <li><Link to="/Level3"><button>Level 3</button></Link></li>
-                <li><Link to="/Level4"><button>Level 4</button></Link></li>
-                </ul>
+              <div className="button-shadow">
+                
+                <Link to="/PlacementTest"><button className="image1-buttons"><img src="/lp.gif" alt="Pre Test" /></button></Link>
+                <Link to="/Level1"><button className="image-buttons"><img src="/l1.gif" alt="Level 1" /></button></Link>
+                <Link to="/level2"><button className="image-buttons"><img src="/l2.gif" alt="Level 2" /></button></Link>
+                <Link to="/Level3"> <button className="image4-buttons"><img src="/l3.gif" alt="Level 3" /></button></Link>
+                <Link to="/Level4"><button className='image4-buttons'><img src="/l4.gif" alt="Level 4"/></button></Link>
+                
+                <button className="background-button" onClick={handleBackgroundChange}>Change Background</button>
             </div>
             </div>
-            <div className="image">
-              <img src="/climb.png" alt="Climbing" />
-            </div>
-            <div className="image2">
-              <img src="/ladder.png" alt="Ladder" />
-  </div>
+            
           </div>
         </header>
       </div>
