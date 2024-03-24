657ca608dd9e92b0f400641ba0ece70cdccbdc14
sarinihasangee <157481833+sarinihasangee@users.noreply.github.com>
2024-03-25 00:49:42 +0530

    Remove unwanted code

--- lexilearn/src/NavBar.js
+++ lexilearn/src/NavBar.js
@@ -39,13 +39,7 @@ function Navbar() {
           <Link to="/SelectLevelsPage">Levels</Link>
           <Link to="/UserForm">User Profile</Link>
           <Link to="/Progress">Progress</Link>
-          {/*<Link to="/LoginPage">Sign In</Link>*/}
-          {/*{currentUser ? (
-          <span>Welcome, {username}</span>
           
-        ) : (
-          <Link to="/LoginPage">Login</Link>
-        )}*/}
 
           {currentUser && <span>Welcome, {currentUser.email}</span>}
           {currentUser && <Link to="#" onClick={handleSignOut}>Sign Out</Link>}
@@ -57,12 +51,6 @@ function Navbar() {
         <Link to="/SelectLevelsPage">Levels</Link>
         <Link to="/UserForm">User Profile</Link>
         <Link to="/Progress">Progress</Link>
-        {/*<Link to="/LoginPage">Sign In</Link>*/}
-        {/*{currentUser ? (
-          <span>Welcome, {username}</span>
-        ) : (
-          <Link to="/LoginPage">Login</Link>
-        )}*/}
 
         {currentUser && <span>Welcome, {currentUser.email}</span>}
         {currentUser && <Link to="#" onClick={handleSignOut}>Sign Out</Link>}
